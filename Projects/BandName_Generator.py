0d49d6d6456f447dd4d4b97d456b29cf1d4f7a2e
Prajyot Mulik <mulikprajyot41@gmail.com>
2025-07-07 13:07:19 +0530

    Day 2

---
+++ Projects/BandName_Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band Name Generator.")
+
+a = input("Whats the name of city you are grew up in ?")
+b = input("Whats your pet name ?")
+
+print("Your band name should be "+ a + " " + b)
